61e307df5a6b35839113294402ff75ff9f44fb79
Makungu <makungu.c200@gmail.com>
2024-07-21 20:06:52 +0200

    updated hurtbox to check for collision groups

--- shared_components/hurt_box.gd
+++ shared_components/hurt_box.gd
@@ -2,5 +2,5 @@ class_name HurtBox
 extends Area2D
 
 func _on_area_entered(area):
-	if owner.has_method("take_damage"):
+	if area.is_in_group("enemy") && owner.has_method("take_damage"):
 		owner.take_damage(area.damage)
